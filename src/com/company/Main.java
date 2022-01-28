53fb328214643ac3869fd52c7e019b34392dc91d
RamzanKi <sqrcloud@gmail.com>
2022-01-28 14:02:35 +0300

    final refactoring

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,31 +10,36 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader systemBr = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("""
-                Зашифровать файл 1
-                Расшифровать файл 2""");
-        String verdict = systemBr.readLine();
-        if (verdict.equals("1")) {
-            new Encoder().encodeText();
-        }
-        if (verdict.equals("2")) {
+        while (true) {
             System.out.println("""
-                    Для декодирования при помощи ключа введите 1
-                    Для использования BruteForce введите 2
-                    Для использования statisticalAnalysis введите 3""");
-            int method = Integer.parseInt(systemBr.readLine());
+                    Зашифровать файл 1
+                    Расшифровать файл 2
+                    Выход 3""");
+            String verdict = systemBr.readLine();
+            if (verdict.equals("1")) {
+                new Encoder().encodeText();
+            }
+            if (verdict.equals("2")) {
+                System.out.println("""
+                        Для декодирования при помощи ключа введите 1
+                        Для использования BruteForce введите 2
+                        Для использования statisticalAnalysis введите 3""");
+                int method = Integer.parseInt(systemBr.readLine());
+                if (method == 1) {
+                    System.out.println("Введите ключ кодирования: ");
+                    int decodeKey = Integer.parseInt(systemBr.readLine());
+                    new Decoder().decodeText(decodeKey);
 
-            if (method == 1) {
-                System.out.println("Введите ключ кодирования: ");
-                int decodeKey = Integer.parseInt(systemBr.readLine());
-                new Decoder().decodeText(decodeKey);
+                } else if (method == 2) {
+                    new Decoder().etTuBrute();
 
-            } else if (method == 2) {
-                new Decoder().etTuBrute();
+                } else if (method == 3) {
+                    new Decoder().statisticalAnalysis();
 
-            } else if (method == 3) {
-                new Decoder().statisticalAnalysis();
-
+                }
+            }
+            if (verdict.equals("3")){
+                System.exit(0);
             }
         }
     }
